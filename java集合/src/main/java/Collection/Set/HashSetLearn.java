11a558467958e0ad1e35c83a19531e12210aa7ce
nino <ninoJay@foxmail.com>
2023-05-31 17:54:20 +0800

    🪣基于手册的集合部分完成

--- java集合/src/main/java/Collection/Set/HashSetLearn.java
+++ java集合/src/main/java/Collection/Set/HashSetLearn.java
@@ -1,8 +1,15 @@
 package Collection.Set;
 
+import java.util.HashSet;
+
 /**
+ * 内部就是一个HashMap
+ * 用键值key的唯一实现Set唯一
+ * 而value则是用内部new了一个Present Object常量来存储
  * @author zengzhongjie
  * @date 2023/5/31
  */
 public class HashSetLearn {
+    private static HashSet<String> hashSet = new HashSet<>();
+
 }
